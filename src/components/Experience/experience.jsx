7961967b9ba903448ce0b2ce255466d4c98640ed
Chong Xern <104547209+ChongXern@users.noreply.github.com>
2026-02-02 13:34:50 +0800

    Add skewed effect inspired from Leetcode

--- src/components/Experience/experience.jsx
+++ src/components/Experience/experience.jsx
@@ -5,6 +5,28 @@ import { getImageUrl } from '../../utils';
 import styles from "./experience.module.css"
 
 export const Experience = () => {
+    // let 
+    const handleMouseMove = (e) => {
+        // el.style.zIndex = "100";
+        const el = e.currentTarget; // get <li> 
+        const rect = el.getBoundingClientRect(); //size and position relative to viewpoint
+
+        const dx = e.clientX - (rect.left + rect.width / 2);
+        const dy = e.clientY - (rect.top + rect.height / 2);
+
+        el.style.setProperty("--tx", `${-dx * 0.03}px`);
+        el.style.setProperty("--ty", `${-dy * 0.03}px`);
+        el.style.setProperty("--skx", `${dx * 0.003}deg`);
+        el.style.setProperty("--sky", `${dy * 0.003}deg`);
+    };
+    const handleMouseLeave = (e) => {
+        const el = e.currentTarget;
+        el.style.setProperty("--tx", "0px");
+        el.style.setProperty("--ty", "0px");
+        // el.style.setProperty("--skx", "0deg");
+        // el.style.setProperty("--sky", "0deg");
+    };
+
     return <section id='experience' className={styles.container}>
         <div className={styles.topBlur}></div>
         <div className={styles.bottomBlur}></div>
@@ -22,7 +44,8 @@ export const Experience = () => {
             }</div>
             <ul className={styles.history}>{
                 history.map((historyItem, id) => {
-                    return <li key={id} className={styles.historyItem}>
+                    return <li key={id} className={styles.historyItem}
+                        onMouseMove={handleMouseMove} onMouseLeave={handleMouseLeave}>
                         <div className={styles.historyItemBox}>
                             <div className={styles.historyItemHeader}>
                                 <h3> {historyItem.role}</h3>
